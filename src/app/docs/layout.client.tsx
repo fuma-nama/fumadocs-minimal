32cd10979123d30c3686d644ad78d53d9792324b
Fuma Nama <god63820869@gmail.com>
2024-12-29 08:43:50 +0800

    fix folders

--- src/app/docs/layout.client.tsx
+++ src/app/docs/layout.client.tsx
@@ -5,16 +5,14 @@ import {
   SidebarMenuSub,
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
-import {
-  Collapsible,
-  CollapsibleTrigger,
-  CollapsibleContent,
-} from "@/components/ui/collapsible";
+import { Collapsible, CollapsibleContent } from "@/components/ui/collapsible";
 import Link from "fumadocs-core/link";
 import type { PageTree } from "fumadocs-core/server";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export function PageTreeItem({ item }: { item: PageTree.Node }) {
+  const [open, setOpen] = useState(false);
   const pathname = usePathname();
 
   if (item.type === "page") {
@@ -31,19 +29,33 @@ export function PageTreeItem({ item }: { item: PageTree.Node }) {
     return <SidebarSeparator>{item.name}</SidebarSeparator>;
 
   if (item.type === "folder") {
+    const onClick = () => {
+      if (!item.index || item.index.url === pathname) {
+        setOpen((prev) => !prev);
+      } else if (item.index && item.index.url !== pathname) {
+        setOpen(true);
+      }
+    };
+
     return (
-      <Collapsible className="group/collapsible">
-        <CollapsibleTrigger asChild>
-          {item.index ? (
-            <SidebarMenuButton isActive={item.index.url === pathname} asChild>
-              <Link href={item.index.url} external={item.index.external}>
-                {item.index.name}
-              </Link>
-            </SidebarMenuButton>
-          ) : (
-            <SidebarMenuButton>{item.name}</SidebarMenuButton>
-          )}
-        </CollapsibleTrigger>
+      <Collapsible
+        open={open}
+        onOpenChange={setOpen}
+        className="group/collapsible"
+      >
+        {item.index ? (
+          <SidebarMenuButton
+            onClick={onClick}
+            isActive={item.index.url === pathname}
+            asChild
+          >
+            <Link href={item.index.url} external={item.index.external}>
+              {item.index.name}
+            </Link>
+          </SidebarMenuButton>
+        ) : (
+          <SidebarMenuButton onClick={onClick}>{item.name}</SidebarMenuButton>
+        )}
         <CollapsibleContent>
           <SidebarMenuSub>
             {item.children.map((child, i) => (
